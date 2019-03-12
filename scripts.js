9653c2c812e7d48c1a40f9554ccb14fbff79002f
Katherine Fleming <katherinemarief@gmail.com>
2019-03-11 20:20:43 -0500

    added dblclick event to div that makes even/odd squares disappear

--- scripts.js
+++ scripts.js
@@ -1,34 +1,49 @@
-window.addEventListener("DOMContentLoaded", function () {
-    let button = document.createElement("button");
-    let btntext = document.createTextNode("Add Square");
-    button.appendChild(btntext);
-    document.body.appendChild(button);
+let button = document.createElement("button");
+let btntext = document.createTextNode("Add Square");
+button.appendChild(btntext);
+document.body.appendChild(button);
+
+let boxdiv = document.createElement("div");
+document.body.appendChild(boxdiv)
+
+button.style.display = "block"
+var idNumber = 0
+button.addEventListener("click", function () {
+    let div = document.createElement("div");
+    div.id = ("id" + idNumber);
+    idNumber++;
+    div.className = "square";
+    boxdiv.appendChild(div);
+    let divtxt = document.createTextNode(idNumber);
+
+    div.addEventListener("mouseover", function () {
+        console.log(idNumber)
+        div.appendChild(divtxt)
+        div.style.textAlign = "center"
+        div.style.lineHeight = "200px"
+        div.style.color = "white"
+    })
+
+    div.addEventListener("mouseout", function () {
+        div.removeChild(divtxt);
+    })
+
+    var colors = ["pink", "green", "turquoise", "orange", "red", "navy"]
+
+    div.addEventListener("click", function () {
+        let randomColor = colors[Math.floor(Math.random() * colors.length)];
+        div.style.backgroundColor = randomColor
+    })
 
 
-    var idNumber = 1
-    button.addEventListener("click", function () {
-        let div = document.createElement("div");
-        div.id = ("id" + idNumber);
-        idNumber++;
-        div.className = "square";
-        document.body.appendChild(div);
+    div.addEventListener("dblclick", function () { //added click event
+        if (idNumber % 2 == 0) {
+            console.log("the square before me will disappear");
+            boxdiv.removeChild(this.previousSibling);
+        } else {
+            console.log("the square after me will disappear");
+            boxdiv.removeChild(this.nextSibling);
+        }
 
-        div.addEventListener("mouseover", function () {
-            console.log(idNumber)
-            let divtxt = document.createTextNode(idNumber);
-            div.appendChild(divtxt)
-            div.style.textAlign = "center"
-        })
-
-        div.addEventListener("mouseout", function () {
-        div.textContent.style.visibility = "hidden";
-        })
-
-        var colors = ["pink", "green", "turquoise", "orange", "red", "navy"]
-
-        div.addEventListener("click", function () {
-            let randomColor = colors[Math.floor(Math.random() * colors.length)];
-            return div.style.backgroundColor = randomColor
-        })
     })
 })
